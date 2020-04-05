869b2dc707c68fb5a684c5d05cbf73b557ee51a5
Peshkov Denis <peshkovdenis1985@gmail.com>
2020-04-05 20:32:18 +0300

    Edited try blocks

--- src/main/java/okey/olga/peshkova/MailParser.java
+++ src/main/java/okey/olga/peshkova/MailParser.java
@@ -4,15 +4,13 @@ import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.search.FlagTerm;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.*;
 
-public class MailParser {
+public final class MailParser {
 
-    private Store store;
+    private MailParser() {
+
+    }
 
     public static class Message {
         private String[] from;
@@ -51,37 +49,33 @@ public class MailParser {
         }
     }
 
-    public Message parse(Path filePath) throws IOException, MessagingException {
-        Message message1 = null;
-
-        try (InputStream inputStream = Files.newInputStream(filePath)) {
-            MimeMessage message = new MimeMessage(Session.getDefaultInstance(new Properties()), inputStream);
-            message1 = new Message();
-            message1.setSubject(message.getSubject());
-            message1.setFrom((InternetAddress[]) message.getFrom());
-        }
-        return message1;
+    @FunctionalInterface
+    private interface ParseFunction {
+        Message[] apply(Store store) throws MessagingException;
     }
 
-    public Message[] parse(String host, String user, String password) throws MessagingException {
+    static public Message[] parse(String host, String user, String password) throws MessagingException {
         return parse(host, user, password, "INBOX");
     }
 
-    public Message[] parse(String host, String user, String password, String mbox) throws MessagingException {
+    static public Message[] parse(String host, String user, String password, String mbox) throws MessagingException {
         return parse(host, user, password, mbox, "imap");
     }
 
-    public Message[] parse(String host, String user, String password, String mbox, String protocol) throws MessagingException {
-        Message[] messages;
-        try (Folder folder = getFolder(host, user, password, mbox, protocol).orElseThrow(() -> new NoSuchElementException("Folder is empty"))) {
-            folder.open(Folder.READ_ONLY);
-            MimeMessage[] messages1 = (MimeMessage[]) folder.search(new FlagTerm(new Flags(Flags.Flag.SEEN), false));
-            messages = Arrays.stream(messages1).map(MailParser::mapMessage).filter(Objects::nonNull).limit(100).toArray(Message[]::new);
-        }
-        return messages;
+    static public Message[] parse(String host, String user, String password, String mbox, String protocol) throws MessagingException {
+
+        return parse(host, user, password, mbox, protocol, store -> {
+            Message[] messages;
+            try (Folder folder = Optional.ofNullable(store.getFolder(mbox)).filter(MailParser::folderExists).orElseThrow(() -> new NoSuchElementException("Folder is empty"))) {
+                folder.open(Folder.READ_ONLY);
+                MimeMessage[] messages1 = (MimeMessage[]) folder.search(new FlagTerm(new Flags(Flags.Flag.SEEN), false));
+                messages = Arrays.stream(messages1).map(MailParser::mapMessage).filter(Objects::nonNull).toArray(Message[]::new);
+            }
+            return messages;
+        });
     }
 
-    private Optional<Folder> getFolder(String host, String user, String password, String mbox, String protocol) throws MessagingException {
+    static private Message[] parse(String host, String user, String password, String mbox, String protocol, ParseFunction function) throws MessagingException {
         Properties props = System.getProperties();
         props.setProperty("mail." + protocol + ".ssl.enable", "true");
 
@@ -91,7 +85,9 @@ public class MailParser {
 
         store.connect(host, user, password);
 
-        return Optional.ofNullable(store.getFolder(mbox)).filter(MailParser::folderExists);
+        try (store) {
+            return function.apply(store);
+        }
     }
 
     private static Message mapMessage(MimeMessage message) {
